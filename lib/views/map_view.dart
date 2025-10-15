38b294e6c6fc6b36b90a2a4a7e9d0b07b635eb42
BUCHACEK Vanessa <vbuchacek@eurofunk.com>
2025-10-15 09:23:23 +0200

    basic map functionality -able to show a basic world map -rework to only show area user selects -additional functionality added later

---
+++ lib/views/map_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MapView extends StatelessWidget {
+  const MapView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(51.509364, -0.128928), // London
+          initialZoom: 9.2,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate:
+            'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+            subdomains: const ['a', 'b', 'c'],
+            userAgentPackageName: 'com.ride2gather.app',
+          ),
+          RichAttributionWidget(
+            attributions: [
+              TextSourceAttribution(
+                '© OpenStreetMap contributors',
+                onTap: () => launchUrl(
+                  Uri.parse('https://www.openstreetmap.org/copyright'),
+                  mode: LaunchMode.externalApplication,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
