8d29007151a68836fdce3b3ba4d8fb53d45dc0cc
BUCHACEK Vanessa <vbuchacek@eurofunk.com>
2025-10-20 16:08:56 +0200

    quick performance fix - I noticed, that when the app is running long/often, the performance gets f*cked - a restart helps but this is not suitable for productive use

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:ride2gather/views/signUp_view.dart';
 
 //this function/class call the app and sets title, theme, color, fonts etc.
 //the first page that get's shown is the signup - will change once user is created and change to login
 void main() {
+  // Make sure binding is initialized so we can tweak the image cache before UI runs
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Quick performance tweak: limit Flutter's in-memory image cache.
+  // These values are conservative and reduce long-term memory use / jank.
+  // Tune them if you have many small images vs. a few large avatars.
+  PaintingBinding.instance.imageCache.maximumSize = 100; // number of images
+  PaintingBinding.instance.imageCache.maximumSizeBytes = 50 * 1024 * 1024; // 50 MB
+
   runApp(const MyApp());
 }
 
